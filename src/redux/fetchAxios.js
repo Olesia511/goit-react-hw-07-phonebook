3901dec631c2af5c16fb6eaa65c5a733764a5442
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-12-16 13:47:56 +0200

    fetch1

--- src/redux/fetchAxios.js
+++ src/redux/fetchAxios.js
@@ -1,19 +1,20 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { fetchingError, fetchingInProgress, fetchingSuccess } from "./contactsSlice";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+// import { fetchingError, fetchingInProgress, fetchingSuccess } from "./contactsSlice";
 
-axios.defaults.baseURL = "<https://657ca06c853beeefdb99bc7a.mockapi.io>";
+axios.defaults.baseURL = '<https://657ca06c853beeefdb99bc7a.mockapi.io>';
 
 // https://657ca06c853beeefdb99bc7a.mockapi.io/contacts/:endpoint
 
-export const fetchContacts = createAsyncThunk("contacts/fetchAll", async (_, thunkAPI) => {
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchAll',
+  async (_, thunkAPI) => {
     try {
-        const response = await axios.get("/contacts");
-        console.log(`response`, response);
+      const response = await axios.get('/contacts');
+      console.log(`response`, response);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
-});
-
-
+  }
+);
